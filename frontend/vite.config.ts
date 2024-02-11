011967e4aa2bc1a106e39e7edfd74d305dd86d3e
Peter Repukat <orly.alia5@gmail.com>
2024-02-11 14:01:39 +0100

    initial commit

---
+++ frontend/vite.config.ts
@@ -0,0 +1,31 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+import Icons from 'unplugin-icons/vite';
+import Unfonts from 'unplugin-fonts/vite';
+
+
+export default defineConfig({
+	plugins: [
+        sveltekit(),
+        Unfonts(),
+        Icons({
+            autoInstall: true,
+            compiler: 'svelte',
+            scale: 1.2,
+            transform: (svg) => svg.replace(/^<svg /, '<svg ')
+        }),
+    ],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
+
+process
+    .on('unhandledRejection', (reason, p) => {
+        console.error(reason, 'Unhandled Rejection at Promise', p);
+        process.exit(1);
+    })
+    .on('uncaughtException', (err) => {
+        console.error(err, 'Uncaught Exception thrown');
+        process.exit(1);
+    });
