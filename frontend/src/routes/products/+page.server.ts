20c592a82feba770f8be3386437c65bec1dc0735
Peter Repukat <orly.alia5@gmail.com>
2024-02-17 18:49:39 +0100

    Add rudimentary pagination

--- frontend/src/routes/products/+page.server.ts
+++ frontend/src/routes/products/+page.server.ts
@@ -1,15 +1,23 @@
 import { medusa } from '$/lib/medusa';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ url }) => {
 
+    const limit = 100;
+    const pageOffset = url.searchParams.get('page')
+        ? (parseInt(url.searchParams.get('page') || '1') - 1) * limit
+        : 0;
     const products = await medusa.products.list({
         currency_code: 'eur',
-        include_category_children: true
+        include_category_children: true,
+        limit,
+        offset: pageOffset
     });
 
     return {
         products: products.products,
+        page: pageOffset + 1,
+        pageCount: Math.ceil(products.count / products.limit),
         crumbs: [
             {
                 name: 'Products',
--- frontend/src/routes/products/+page.svelte
+++ frontend/src/routes/products/+page.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
+import Paginator from '$/lib/components/Paginator.svelte';
 import ProductGallery from '$/lib/components/ProductGallery.svelte';
 import type { PageData } from './$types';
 
 export let data: PageData;
 </script>
 
-<ProductGallery products="{data.products}" />
+<div>
+    <ProductGallery products="{data.products}" />
+    <Paginator page="{data.page}" pageCount="{data.pageCount}" />
+</div>
 
 <style lang="postcss">
 </style>
