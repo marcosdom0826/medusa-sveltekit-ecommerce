d62e578aad511b3d948f416360b871d017d464ef
Marcos <marcosdom0826@gmail.com>
2024-02-20 22:47:05 +0100

    Add ProductImageVIewer

--- frontend/src/routes/products/[...category]/view/[product]/+page.svelte
+++ frontend/src/routes/products/[...category]/view/[product]/+page.svelte
@@ -1,59 +1,25 @@
 <script lang="ts">
-import { onMount } from 'svelte';
 import type { PageData } from './$types';
+import ProductImageView from '$/lib/components/ProductImageView.svelte';
 
 export let data: PageData;
-
-let zoomed = false;
-let imgTranslate = '0 0';
-let pictureEl: HTMLPictureElement | undefined;
-let hasZoomTransitions = false;
-
-const zoomFactor = 2.25;
-
-const zoomMouseHandler = (e: MouseEvent) => {
-    if (!e.target || !(e.target instanceof HTMLElement)) {
-        return;
-    }
-    const x = (e.offsetX / e.target.clientWidth) * 100;
-    const y = (e.offsetY / e.target.clientHeight) * 100;
-    imgTranslate = `${-(x - 50) * (zoomFactor - 1)}% ${-(y - 50) * (zoomFactor - 1)}%`;
-};
-const setZoomed = (e: Event, value: boolean) => {
-    zoomed = value;
-    if (!zoomed) {
-        pictureEl?.removeEventListener('mousemove', zoomMouseHandler);
-        hasZoomTransitions = false;
-        imgTranslate = '0 0';
-    }
-    if (zoomed) {
-        zoomMouseHandler(e as PointerEvent);
-        setTimeout(() => {
-            hasZoomTransitions = true;
-            pictureEl?.addEventListener('mousemove', zoomMouseHandler);
-        }, 250);
-    }
-};
 </script>
 
 <div class="wrapper">
     <div class="product-content">
-        <div class="image-view">
-            <button on:click="{(e) => setZoomed(e, !zoomed)}" on:mouseleave="{(e) => setZoomed(e, false)}">
-                <picture
-                    class="{zoomed ? 'zoomed' : ''} {hasZoomTransitions ? 'zoom-transitions' : ''}"
-                    style="--translate: {imgTranslate}; --scale: {zoomFactor};"
-                    bind:this="{pictureEl}">
-                    <source srcset="{data.product.images?.[0]?.url}" />
-                    <img src="{data.product.images?.[0]?.url}" alt="{data.product.title}" loading="lazy" />
-                </picture>
-            </button>
+        <div class="images-wrapper sticky-desktop">
+            <ProductImageView product="{data.product}" />
         </div>
+
         <div class="product-info">
             <h2>{data.product.title}</h2>
             <h3>{data.product.subtitle}</h3>
             <p>{data.product.description}</p>
         </div>
+    </div>
+    <div class="dummy">
+        <h1>Placeholder</h1>
+        <p>Landing content here</p>
     </div>
 </div>
 
@@ -61,14 +27,30 @@ const setZoomed = (e: Event, value: boolean) => {
 .wrapper {
     display: grid;
     place-items: center;
+    height: 100%;
+}
+
+.dummy {
+    grid-column: 1 / -1;
+    display: grid;
+    place-content: center;
+    height: 6000px;
+    background: firebrick;
+    width: 100%;
 }
 
 .product-content {
     padding: 1rem;
     display: grid;
-    grid-template-columns: 1fr 1fr;
+    @media (orientation: landscape) {
+        grid-template-columns: 1fr 1fr;
+    }
+    @media (orientation: portrait) {
+        grid-template-rows: min-content auto;
+    }
     width: min(100%, 1600px);
-    grid-template-rows: minmax(75dvh, 25dvh);
+    height: 100%;
+    position: relative;
 }
 
 .product-info {
@@ -78,28 +60,21 @@ const setZoomed = (e: Event, value: boolean) => {
     padding: 1rem;
 }
 
-.image-view button {
-    padding: 0;
-    border: none;
-    cursor: zoom-in;
+.images-wrapper {
+    width: 100%;
     overflow: hidden;
+    @media (orientation: portrait) {
+        max-height: 75dvh;
+    }
+    max-height: calc(100dvh - 1.5rem);
+    border-radius: 0.5rem;
 }
 
-.zoomed {
-    object-fit: none;
-    scale: var(--scale);
-    translate: var(--translate);
-}
-
-.zoom-transitions {
-    transition: scale var(--transitionDuration) ease;
-}
-
-picture,
-img {
-    object-fit: contain;
-    transition:
-        scale var(--transitionDuration) ease,
-        translate var(--transitionDuration) ease;
+.sticky-desktop {
+    @media (orientation: landscape) {
+        position: sticky;
+        top: 1em;
+        align-self: start;
+    }
 }
 </style>
