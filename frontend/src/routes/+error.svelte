b1c5026d3aac90f365ebb4f55ed867036f40a73c
Peter Repukat <orly.alia5@gmail.com>
2024-03-02 18:28:25 +0100

    Add template error layout

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { page } from '$app/stores';
+</script>
+
+<div class="wrapper">
+    <div>
+        <h1>{$page?.status}</h1>
+        <h2>{$page.error?.message}</h2>
+    </div>
+</div>
+
+<style lang="postcss">
+.wrapper {
+    position: absolute;
+    inset: 0;
+    padding: 1em;
+    @media (orientation: portrait) {
+        position: static;
+    }
+}
+div {
+    display: grid;
+    place-items: center;
+}
+</style>
