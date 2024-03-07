addae50c80e1bbb6fa42b7f9ae8d9973e929fc10
Peter Repukat <orly.alia5@gmail.com>
2024-03-07 15:53:40 +0100

    Add order-success page

--- frontend/src/routes/checkout/success/[order]/+page.svelte
+++ frontend/src/routes/checkout/success/[order]/+page.svelte
@@ -1,7 +1,191 @@
 <script lang="ts">
+import Cart from '$/lib/components/Cart.svelte';
+import { t } from '$/lib/i18n';
+import type { PageData } from './$types';
+
+export let data: PageData;
+$: shippingCost = data.order.shipping_total || 0;
 </script>
 
-SUCCESS
+<div class="content">
+    <div>
+        <h2>Thank you for your order</h2>
+        <div>
+            <p>Your order has been placed and will be shipped soon.</p>
+            <p>Thank you for shopping with us.</p>
+        </div>
+        <div class="order-info">
+            <h3 class="wide">Contact</h3>
+            <div class="columns contact">
+                <span>Email:</span>
+                <span>{data.cart?.email}</span>
+                {#if data.cart?.shipping_address?.phone}
+                    <span>Phone:</span>
+                    <span>{data.cart?.shipping_address?.phone}</span>
+                {/if}
+            </div>
+            <h3 class="wide">Delivery Address</h3>
+            <div class="columns">
+                <span>
+                    {data.cart?.shipping_address?.first_name}
+                    {data.cart?.shipping_address?.last_name}
+                </span>
+                {#if data.cart?.shipping_address?.company}
+                    <span class="wide">{data.cart?.shipping_address?.company}</span>
+                {/if}
+                <span class="wide">{data.cart?.shipping_address?.address_1}</span>
+                <span>{data.cart?.shipping_address?.postal_code} {data.cart?.shipping_address?.city}</span>
+            </div>
+            {#if data.cart?.billing_address}
+                <h3 class="wide">Billing Address</h3>
+                <div class="columns">
+                    <span>
+                        {data.cart?.billing_address?.first_name}
+                        {data.cart?.billing_address?.last_name}
+                    </span>
+                    {#if data.cart?.billing_address?.company}
+                        <span class="wide">{data.cart?.billing_address?.company}</span>
+                    {/if}
+                    <span class="wide">{data.cart?.billing_address?.address_1}</span>
+                    <span>
+                        {data.cart?.billing_address?.postal_code}
+                        {data.cart?.billing_address?.city}
+                    </span>
+                </div>
+            {/if}
+
+            <h3 class="wide">Shipping method</h3>
+            <div class="columns">
+                <span>{data.cart?.shipping_methods?.[0]?.shipping_option?.name}</span>
+                <span>{shippingCost > 0 ? `${shippingCost / 100}€` : 'Free'}</span>
+            </div>
+        </div>
+        <div class="rhs">
+            <Cart items="{data.order.items}" disableEdit>
+                <div slot="total" class="total">
+                    <h4>Subtotal:</h4>
+                    <span>{(data.order?.subtotal || 0) / 100}€</span>
+                    <span>Shipping:</span>
+                    <span>{shippingCost === 0 ? 'Free' : `${shippingCost / 100}€`}</span>
+                    <h3>Total:</h3>
+                    <span>{data.order.total / 100}€</span>
+                    <h3>Payment:</h3>
+                    <span>{$t(`payment.${data.order.payments?.[0].provider_id}`)}</span>
+                    {#if data.order.payments?.[0].provider_id === 'manual'}
+                        <p>
+                            <!-- eslint-disable prettier/prettier -->
+                            You will receive an invoice with payment instructions via e-email
+                            Please wire the money withing 7 business days
+                            <!-- eslint-enable prettier/prettier -->
+                        </p>
+                    {/if}
+                </div>
+            </Cart>
+        </div>
+    </div>
+</div>
 
 <style lang="postcss">
+.content {
+    width: min(100%, 1800px);
+    justify-self: center;
+    display: grid;
+    justify-content: center;
+    padding: 1em;
+    height: fit-content;
+
+    /* @media (orientation: landscape) {
+        &::after {
+            content: '';
+            position: absolute;
+            top: 1em;
+            bottom: 1em;
+            right: 50%;
+            width: 1px;
+            opacity: 0.25;
+            background-color: var(--textColor);
+        }
+    } */
+
+    & > div {
+        display: grid;
+        gap: 2em;
+        width: 100%;
+
+        & > :first-child {
+            grid-column: 1 / -1;
+            text-align: center;
+        }
+        & > :nth-child(2) {
+            grid-column: 1 / -1;
+            text-align: center;
+        }
+        @media (orientation: portrait) {
+            grid-template-columns: 100%;
+            width: 100%;
+            overflow-x: hidden;
+        }
+
+        grid-template-columns: repeat(2, 1fr);
+        @media (orientation: landscape) {
+            & > * {
+                width: 100%;
+            }
+        }
+    }
+}
+
+.rhs {
+    display: grid;
+    gap: 1em;
+    height: fit-content;
+}
+
+.order-info {
+    & > :first-child {
+        margin-top: 0;
+    }
+    & h3 {
+        margin-top: 1em;
+    }
+
+    & > .columns {
+        padding: 1em;
+    }
+    & > .contact {
+        gap: 0.5em;
+        & > :nth-child(odd) {
+            font-weight: bold;
+        }
+    }
+}
+
+.wide {
+    grid-column: 1 / -1;
+}
+
+.columns {
+    display: grid;
+    grid-template-columns: auto min-content;
+    padding: 0;
+
+    @media (orientation: portrait) {
+        overflow-x: hidden;
+    }
+}
+
+.total {
+    display: grid;
+    grid-template-columns: auto min-content;
+    width: 100%;
+    gap: 1em;
+    align-items: center;
+
+    & span {
+        font-weight: bold;
+    }
+    & > :nth-child(even) {
+        text-align: end;
+    }
+}
 </style>
