055c4476a02f5462db807d8401698deef0215888
Peter Repukat <orly.alia5@gmail.com>
2024-03-18 15:26:28 +0100

    Add gift-card display-page for emails

---
+++ frontend/src/routes/gift-card/[code]/+page.server.ts
@@ -0,0 +1,26 @@
+import { medusa } from '$/lib/medusa';
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+
+export const load: PageServerLoad = async ({ params }) => {
+
+    let giftCard;
+    try {
+        giftCard = await medusa.giftCards.retrieve(params.code);
+        if (giftCard.gift_card.balance <= 0) {
+            throw error(404, 'Not found');
+        }
+        if (giftCard.gift_card.is_disabled) {
+            throw error(404, 'Not found');
+        }
+    } catch (e) {
+        throw error(404, 'Not found');
+    }
+
+    return {
+        code: params.code,
+        balance: giftCard.gift_card.balance
+    };
+
+};
---
+++ frontend/src/routes/gift-card/[code]/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+import type { PageData } from './$types';
+
+export let data: PageData;
+</script>
+
+<div class="wrapper">
+    <div>
+        <h3>Thank you for your order!</h3>
+        <h3>Your {data.balance / 100} € gift card code is</h3>
+        <h2>{data.code}</h2>
+    </div>
+</div>
+
+<style lang="postcss">
+.wrapper {
+    display: grid;
+    place-items: center;
+    height: 100%;
+    & > div {
+        display: grid;
+        justify-items: center;
+        gap: 1em;
+    }
+}
+
+span {
+    font-weight: bold;
+}
+</style>
